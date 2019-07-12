39bda0f334ae70b90c0a9717e011b109c76cf0d3
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-12 02:34:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,3 +13,4 @@ def introduction_with_language_optional(name, options{language,"Ruby")
 
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_language("Caitlin")
